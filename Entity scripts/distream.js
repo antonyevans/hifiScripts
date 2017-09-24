66f6160b012c13d4a24e1ebab874f0d491dceb86
antonyevans <antonyevans@gmail.com>
2017-09-23 22:59:48 -0700

    fix debug

--- Entity scripts/distream.js
+++ Entity scripts/distream.js
@@ -3,6 +3,14 @@
 //  
 
 (function(){
+    
+    var WANT_DEBUG = true;
+    function debugPrint(string) {
+        if (WANT_DEBUG) {
+            print("distream | " + string);
+        }
+    }
+    
     debugPrint("able to write to debugger")
 
 })
